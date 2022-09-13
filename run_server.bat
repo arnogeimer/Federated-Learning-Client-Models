fa084bd31fdc70954259177e91dc145d7cedeafc
Arno Michel Denis GEIMER <arno.geimer@uni.lu>
2022-09-13 11:31:18 +0200

    Moved Files to new project

--- run_server.bat
+++
@@ -1,6 +0,0 @@
-start cmd /k C:\Users\arno.geimer\AppData\Local\Programs\Python\Python310\python.exe C:\Users\arno.geimer\Desktop\Paper\code\GLM_server.py 
-for /l %%x in (1, 1, 5) do (
-start cmd /k C:\Users\arno.geimer\AppData\Local\Programs\Python\Python310\python.exe C:\Users\arno.geimer\Desktop\Paper\code\GLM_client.py 
-)
-%* 
-pause
